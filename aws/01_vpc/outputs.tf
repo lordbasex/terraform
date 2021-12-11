b691adbe027202750d2c0583470fc7b4ea7e9b94
root <root@ip-172-31-91-160.ec2.internal>
2021-12-11 20:13:44 +0000

    init

---
+++ aws/01_vpc/outputs.tf
@@ -0,0 +1,11 @@
+# VPC
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+# Subnets
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
---
+++ aws/01_vpc/terraform.tfvars
@@ -0,0 +1,3 @@
+project_name = "VPC-TESTING-01"
+environment  = "testing"
+region       = "us-east-1"
