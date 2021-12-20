d4eedda24a304dbda7ca7698a597bf4bb8e2d47b
Federico Pereira <lord.basex@gmail.com>
2021-12-20 16:59:08 -0300

    Create variables.tf

---
+++ aws/00_s3/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  description = "Everything for state related terraform"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = ""
+}
