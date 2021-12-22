809189ada02b4c9ac9954532be6d39f47b5956aa
Federico Pereira <lord.basex@gmail.com>
2021-12-22 10:12:39 -0300

    Update variables.tf

--- aws/02_vpc-variables/variables.tf
+++ aws/02_vpc-variables/variables.tf
@@ -5,13 +5,13 @@
 variable "project_name" {
   description = "Project name"
   type        = string
-  default     = "VPC-TESTING-01"
+  default     = "VPC-MyProjectName"
 }
 
 variable "environment" {
   description = "Environment"
   type        = string
-  default     = "testing"
+  default     = "Production"
 }
 
 variable "aws_region" {
@@ -23,26 +23,26 @@ variable "aws_region" {
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
-  default     = "172.17.0.0/16"
-}
-
-variable "private_subnet_cidr_blocks" {
-  description = "Available cidr blocks for private subnets."
-  type        = list(string)
-  default = [
-    "172.17.1.0/24",
-    "172.17.2.0/24",
-    "172.17.3.0/24",
-  ]
+  default     = "172.31.0.0/16"
 }
 
 variable "public_subnet_cidr_blocks" {
   description = "Available cidr blocks for public subnets."
   type        = list(string)
   default = [
-    "172.17.51.0/24",
-    "172.17.52.0/24",
-    "172.17.53.0/24",
+    "172.31.1.0/24",
+    "172.31.2.0/24",
+    "172.31.3.0/24",
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  description = "Available cidr blocks for private subnets."
+  type        = list(string)
+  default = [
+    "172.31.31.0/24",
+    "172.31.32.0/24",
+    "172.31.33.0/24",
   ]
 }
 
@@ -50,9 +50,9 @@ variable "intra_subnet_cidr_blocks" {
   description = "Available cidr blocks for intra subnets."
   type        = list(string)
   default = [
-    "172.17.101.0/24",
-    "172.17.102.0/24",
-    "172.17.103.0/24",
+    "172.31.61.0/24",
+    "172.31.62.0/24",
+    "172.31.63.0/24",
   ]
 }
 
